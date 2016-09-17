c36783d320de0fac036542fd75b0f95227fda8b9
xelita <admin@xelita.com>
2016-09-17 19:12:58 +0200

    scripts to build all docker images

---
+++ docker_build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+docker_build () {
+    echo "building "$1
+    build_dir=$current_dir"/"$1
+    echo $1" dir is:" $build_dir
+    cd $build_dir && mvn docker:build
+}
+
+docker_build_all () {
+    docker_build "discovery-management"
+}
+
+current_dir=$(pwd)
+echo "current_dir is:" $current_dir
+
+docker_build_all
